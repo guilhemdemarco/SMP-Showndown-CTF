3271627129b70da952f1c015afb6d07d76c5f654
nekk <108535017+iam-nekk@users.noreply.github.com>
2024-09-04 19:40:40 +0200

    set up base plugin

---
+++ src/main/java/com/discord/smpshowdown/cTF/CTF.java
@@ -0,0 +1,18 @@
+package com.discord.smpshowdown.cTF;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CTF extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
